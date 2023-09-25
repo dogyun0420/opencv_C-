821e3a2c1287d8037f15afc5661a0f6b9a85eefb
dogyun0420 <bogyun0420@gmail.com>
2023-09-25 10:33:15 +0900

    09/25

--- ch04/ch04_1.cpp
+++ ch04/ch04_1.cpp
@@ -41,13 +41,54 @@ void camera_in()
     destroyAllWindows();
 }
 
+void camera_in_video_out()
+{
+    VideoCapture cap(0);
+    
+    if (!cap.isOpened()){
+        cerr << "Camera open failed!" << endl;
+        return;
+    }
+    
+    int w = cvRound(cap.get(CAP_PROP_FRAME_WIDTH));
+    int h = cvRound(cap.get(CAP_PROP_FRAME_HEIGHT));
+    double fps = cap.get(CAP_PROP_FPS);
+    
+    int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+    int delay = cvRound(1000 / fps);
+    
+    VideoWriter outputVideo("output.avi", fourcc, fps, Size(w,h));
+    
+    if (!outputVideo.isOpened()){
+        cout << "File open failde!" << endl;
+        return;
+    }
+    
+    Mat frame, inversed;
+    while (true) {
+        cap >> frame;
+        if (frame.empty())
+            break;
+        inversed = ~frame;
+        outputVideo << inversed;
+        
+        imshow("frame", frame);
+        imshow("inversed", inversed);
+        
+        if (waitKey(delay) == 27)
+            break;
+    }
+    destroyAllWindows();
+    
+}
 int main()
 {
     
     //MatCless();
     //Matcopy();
     //ScalarOp();
-    camera_in();
+    //camera_in();
+    camera_in_video_out();
     return 0;
 }
 
---
+++ ch05/ch05_ilne.hpp
@@ -0,0 +1,13 @@
+//
+//  ch05_ilne.hpp
+//  opencv
+//
+//  Created by 김도균 on 2023/09/25.
+//
+
+#ifndef ch05_ilne_hpp
+#define ch05_ilne_hpp
+
+#include <stdio.h>
+
+#endif /* ch05_ilne_hpp */
